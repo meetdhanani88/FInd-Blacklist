173db4cd89b6bc86404aa71443d69752839f56f1
ketansali <c100-29@C100-29s-iMac.local>
2022-05-27 17:45:05 -0700

    image bug fix

--- backend/src/controller/Vendor.controller.js
+++ backend/src/controller/Vendor.controller.js
@@ -4,17 +4,17 @@ const fs = require('fs')
 const Path = require('path')
 exports.VendorPendingReq =async (req,res)=>{
     const {vendorName,Address,ReasonForUser} = req.body
-    
+    const vendorValues = {
+        vendorName,Address,ReasonForUser,
+        Requested_User : req.user.user,
+        Requested_Status : 'Pending',
+    }
+    if(req.file){
+        vendorValues.image = req.file.filename
+    }
    
     try{
-        const vendor = await Vendor({
-            vendorName,
-            Address,
-            ReasonForUser,
-            Requested_User : req.user.user,
-            Requested_Status : 'Pending',
-            image : req.file.filename
-        })
+        const vendor = await Vendor(vendorValues)
         
         await vendor.save((err,vendor)=>{
             if(err) return res.status(400).json(err)
@@ -92,7 +92,7 @@ exports.AddToBlackList =async (req,res)=>{
             if(err) return res.status(400).json(err)
             if(vendor){
                 return res.status(200).json({
-                    message : 'Successfully sent Request to Admin',
+                    message : 'Successfully Added to Blacklist',
                     vendor : vendor
                 })
             }
@@ -129,21 +129,7 @@ exports.BlackListVendorUpdate =async (req,res)=>{
      }catch(err){
          return res.status(400).json(err)
      }
-    // try{
-    //    const updatedVendor = await Vendor.findByIdAndUpdate(id,{
-    //     vendorName,
-    //     Address,
-    //     ReasonForAdmin
-    //    }, { new: true })
-    //    if(updatedVendor){
-    //     return res.status(200).json({
-    //         message: "updated",
-    //         vendor: updatedVendor,
-    //     });
-    //    }
-    // }catch(err){
-    //     return res.status(400).json(err)
-    // }
+    
 
 }
 
