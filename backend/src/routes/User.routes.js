d394f450c8e7c8faad239d6ef938dbd7b866ce7a
C223 <c223@C223s-iMac.local>
2022-05-31 10:56:15 +0530

    merge

--- backend/src/routes/User.routes.js
+++ backend/src/routes/User.routes.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const { SignUp, SignIn, ResetPassword, forGotPassword, createUser, getAllUsers, deleteUser, updateUser, inActivePlan, ActivePlan, createRole, userActiveOrInActive } = require('../controller/User.controller')
-=======
-const { SignUp, SignIn, ResetPassword, forGotPassword, createUser, getAllUsers, deleteUser, updateUser, inActivePlan, ActivePlan, auth } = require('../controller/User.controller')
->>>>>>> dev
 const { requireSignIn } = require('../middleware')
 
 const router = require('express').Router()
@@ -15,12 +11,6 @@ router.post('/user/createUser', requireSignIn, createUser)
 router.get('/user/getAllUsers', requireSignIn, getAllUsers)
 router.post('/user/deleteUser/:id', requireSignIn, deleteUser)
 router.post('/user/updateUser/:id', requireSignIn, updateUser)
-<<<<<<< HEAD
 router.post('/user/userActiveOrInActive/:id', requireSignIn, userActiveOrInActive)
-=======
-router.post('/user/inActivePlan/:id', requireSignIn, inActivePlan)
-router.post('/user/ActivePlan/:id', requireSignIn, ActivePlan)
-router.get('/user/auth', requireSignIn, auth)
->>>>>>> dev
 
 module.exports = router
